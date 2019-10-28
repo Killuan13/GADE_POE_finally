4717650a5daaf1d7de9c4bd538c7d93c28b2389e
Killuan13 <55444256+Killuan13@users.noreply.github.com>
2019-10-28 15:40:58 +0200

    last push

--- GADE_POE_final/Assets/Scripts/WizardUnit.cs
+++ GADE_POE_final/Assets/Scripts/WizardUnit.cs
@@ -14,19 +14,7 @@ public class WizardUnit : Unit
         atk = 4;
         range = 2;
         spd = 1;
-        team = Random.Range(1, 4);
         GetComponent<MeshRenderer>().material = mat[team - 1];
-        switch (team)
-        {
-            case 1:
-                gameObject.tag = "team 1";
-                break;
-            case 2:
-                gameObject.tag = "team 2";
-                break;
-            case 3:
-                gameObject.tag = "team 3";
-                break;
-        }
+        gameObject.tag = "team 3";
     }
 }
